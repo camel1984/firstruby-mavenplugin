6df8bc06846713d987283d54be9ac8a8fef0ca6a
shuaijie <shuaijie@amazon.com>
2020-01-03 06:51:09 -0800

    retrieve plexus component from container

---
+++ src/main/scripts/testplexus.rb
@@ -0,0 +1,14 @@
+# This mojo tests plexus integration
+# @goal testplexus
+# @phase validate
+class TestPlexus < Mojo
+# @parameter type="org.codehaus.plexus.archiver.Archiver" expression = "${component.org.codehaus.plexus.archiver.Archiver#zip}"
+
+  def archiver
+  end
+
+  def execute
+    info $archiver.inspect
+  end
+end
+run_mojo TestPlexus
